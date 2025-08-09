1afef80d190c7a501003fd7096ef9b3d6c15960b
Trần Hữu Nhân <52300235@student.tdtu.edu.vn>
2025-08-09 22:38:07 +0700

    update

--- views/profile.php
+++ views/profile.php
@@ -60,12 +60,8 @@ switch (strtolower($userData['role'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="POS - Bootstrap Admin Template">
-    <meta name="keywords"
-        content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects">
-    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
-    <meta name="robots" content="noindex, nofollow">
     <title>Trang hồ sơ cá nhân</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicon.svg">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
